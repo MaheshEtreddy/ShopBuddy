275c434da6cf359510fcaa2c8e05ff1d456b5d0e
vamsipradeep <vamsiprad@gmail.com>
2014-07-21 16:44:42 +0530

    admin

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<?php 
+include_once 'header.php';
+?>
+<div class="row-fluid">
+    
+    <!--/span3-->
+
+    <div class="span10">
+
+        <div>
+            <h1>Hello, World!</h1>
+
+            <p>
+                A super admin interface!
+               
+            </p>
+        </div>
+
+    </div>
+    <!--/span9-->
+
+</div>
+<!--/row-fluid-->
+
+<hr>
+<?php include_once 'footer.php';?>
