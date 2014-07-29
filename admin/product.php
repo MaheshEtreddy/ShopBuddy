34586fdfcdf96f93abd1aebb52693bd2b2bcf6a2
MaheshEtreddy <mahesh.etreddy@pbsystems.com>
2014-07-29 13:16:58 +0530

    Mahesh commit

--- admin/product.php
+++ admin/product.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['err']) == 'updated') {
 		<strong>User</strong>
 		has been deleted!.
 		</div>";
-	unset($_SESSION['err']);
+	unset($_SESSION['err']); 
 }elseif (isset($_SESSION['err']) == 'not deleted'){
 	echo "<div class='alert alert-error'>
 		
